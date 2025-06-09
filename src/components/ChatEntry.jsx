1366ee8f3991f75a379f6eb17268aa0790cb488c
Jamie Hendrickson <jamie.o.hendrickson@gmail.com>
2025-06-09 15:01:12 -0700

    Sets up project, implements ChatEntry component to display sender, body, and timestamp

--- src/components/ChatEntry.jsx
+++ src/components/ChatEntry.jsx
@@ -1,20 +1,26 @@
+import PropTypes from 'prop-types';
+import TimeStamp from './TimeStamp';
 import './ChatEntry.css';
 
-const ChatEntry = () => {
+const ChatEntry = (props) => {
   return (
-    <div className="chat-entry local">
-      <h2 className="entry-name">Replace with name of sender</h2>
-      <section className="entry-bubble">
-        <p>Replace with body of ChatEntry</p>
-        <p className="entry-time">Replace with TimeStamp component</p>
-        <button className="like">🤍</button>
+    <div className='chat-entry local'>
+      <h2 className='entry-name'>{props.sender}</h2>
+      <section className='entry-bubble'>
+        <p>{props.body}</p>
+        <p className='entry-time'>
+          <TimeStamp time={props.timeStamp} />
+        </p>
+        <button className='like'>🤍</button>
       </section>
     </div>
   );
 };
 
 ChatEntry.propTypes = {
-  // Fill with correct proptypes
+  sender: PropTypes.string.isRequired,
+  body: PropTypes.string.isRequired,
+  timeStamp: PropTypes.string.isRequired,
 };
 
 export default ChatEntry;
